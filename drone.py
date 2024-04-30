248a1c7fccd7bf46f2b2160839f2dd6300b99fa8
Or Ayubi <orayubi89@gmail.com>
2024-04-30 04:12:32 +0300

    changed things to be simpler

--- drone.py
+++ drone.py
@@ -13,30 +13,52 @@ class Drone:
                                                                   self.connection.target_component))
 
     def arm(self):
+        # using my own implementation and not the builtin so i can throw an error
         response = self._send_command(command_id=mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, param1=1)
         print(response)
+        self.connection.motors_armed_wait()
 
-    def takeoff(self, altitude, longitude=0, latitude=0, yaw=1):
-        response = self._send_command(command_id=mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, param4=yaw, param5=latitude, param6=longitude, param7=altitude)
+    def takeoff(self, altitude, longitude=0, latitude=0, yaw=0):
+        response = self._send_command(command_id=mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, param4=yaw, param5=latitude,
+                                      param6=longitude, param7=altitude)
         print(response)
 
     def change_altitude(self, altitude, delay=1):
         self.goto(altitude=altitude, delay=delay)
 
     def land(self, latitude=0, longitude=0, altitude=0, yaw=0):
-        response = self._send_command(command_id=mavutil.mavlink.MAV_CMD_NAV_LAND, param4=yaw, param5=latitude, param6=longitude, param7=altitude)
+        response = self._send_command(command_id=mavutil.mavlink.MAV_CMD_NAV_LAND, param4=yaw, param5=latitude,
+                                      param6=longitude, param7=altitude)
         print(response)
 
-    def goto(self, latitude=0, longitude=0, altitude=0, delay=1):
-        response = self._send_command(command_id=mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, param1=delay,param5=latitude, param6=longitude, param7=altitude)
-        print(response)
+    def goto(self, latitude=0, longitude=0, altitude=0, delay=0):
+        self.connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_global_int_message(
+            0,  # time since sender boot - doesnt matter
+            self.connection.target_system,
+            self.connection.target_component,
+            mavutil.mavlink.MAV_FRAME_GLOBAL_TERRAIN_ALT,  # altitude will be relative to terrain
+            int(0b110111111000),  # type mask - only use given position
+            int(latitude * 10 ** 7),  # desired latitude, multiplied by 10**7
+            int(longitude * 10 ** 7),  # desired longitude, multiplied by 10**7
+            altitude,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ))
+        # print(response)
 
     def get_location(self):
-        response = self._req_message_get_response(message_type='GLOBAL_POSITION_INT', message_id=mavutil.mavlink.MAVLINK_MSG_ID_GLOBAL_POSITION_INT)
-        print(response)
+        print(self.connection.location())
+        return self.connection.location()
 
     def get_speed(self):
-        response = self._req_message_get_response(message_type='CONTROL_SYSTEM_STATE', message_id=mavutil.mavlink.MAVLINK_MSG_ID_CONTROL_SYSTEM_STATE)
+        response = self._req_message_get_response(message_type='CONTROL_SYSTEM_STATE',
+                                                  message_id=mavutil.mavlink.MAVLINK_MSG_ID_CONTROL_SYSTEM_STATE)
         print(response)
 
     def get_battery(self):
@@ -44,10 +66,10 @@ class Drone:
         print(response)
 
     def is_armed(self):
-        self.connection.motors_armed()
+        return self.connection.motors_armed()
 
     def _send_command(self, command_id, param1=0, param2=0, param3=0, param4=0, param5=0, param6=0, param7=0):
-        '''
+        """
         Send a command to the drone as a COMMAND_LONG
         :param command_id:
         :param param1:
@@ -58,7 +80,9 @@ class Drone:
         :param param6:
         :param param7:
         :return:
-        '''
+        """
+        print(f"sending command {command_id}")
+
         message = self.connection.mav.command_long_encode(
             self.connection.target_system,  # Target system ID
             self.connection.target_component,  # Target component ID
@@ -80,11 +104,14 @@ class Drone:
 
         return response
 
-    def _request_message(self, message_id, req_param1=0, req_param2=0, req_param3=0, req_param4=0, req_param5=0, response_target=0):
-        self._send_command(mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, param1=message_id, param2=req_param1, param3=req_param2, param4=req_param3, param5=req_param4, param6=req_param5, param7=response_target)
+    def _request_message(self, message_id, req_param1=0, req_param2=0, req_param3=0, req_param4=0, req_param5=0,
+                         response_target=0):
+        self._send_command(mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, param1=message_id, param2=req_param1,
+                           param3=req_param2, param4=req_param3, param5=req_param4, param6=req_param5,
+                           param7=response_target)
 
-    def _req_message_get_response(self, message_type, message_id, req_param1=0, req_param2=0, req_param3=0, req_param4=0, req_param5=0, response_target=0):
+    def _req_message_get_response(self, message_type, message_id, req_param1=0, req_param2=0, req_param3=0,
+                                  req_param4=0, req_param5=0, response_target=0):
         self._request_message(message_id, req_param1, req_param2, req_param3, req_param4, req_param5, response_target)
         response = self.connection.recv_match(type=message_type, blocking=True)
         return response
-
--- main.py
+++ main.py
@@ -8,8 +8,29 @@ ip = "172.19.16.1"
 port = 14550
 
 
+'''
+TODO:
+1. make values returned by drone be good
+2. throw errors from drone if command failed
+3. create chat client
+4. map drone method to command in commandreciever
+5. 
+'''
+
+
 drone = Drone(ip_address=ip, port=port)
-#drone.arm()
-#time.sleep(5)
+drone.arm()
+time.sleep(5)
+drone.takeoff(10)
+drone.get_location()
+#drone.goto(-35.363240,149.2,20)
+#drone.get_speed()
+drone.is_armed()
+drone.get_battery()
+drone.change_altitude(15)
+drone.get_location()
+drone.get_speed()
+drone.land()
+
 drone.get_speed()
 #manager = DroneCommandReceiver("1", "1", ip, port,)
